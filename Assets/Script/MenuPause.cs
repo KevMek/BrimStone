a7a17180efee2674d73090e8eb17fcf73a816e08
KevMek <kevinmendoza78@gmail.com>
2025-01-16 16:35:09 -0500

    adds the new scene with its funtions

---
+++ Assets/Script/MenuPause.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+    [SerializeField] private GameObject menuPause;
+    bool gameIsPause = false;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameIsPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Pause()
+    {
+        gameIsPause = true;
+        Time.timeScale = 0f;
+        menuPause.SetActive(true);
+        Debug.Log("Pause game");
+    }
+
+    public void Resume()
+    {
+        gameIsPause = false;
+        Time.timeScale = 1f;
+        menuPause.SetActive(false);
+        Debug.Log("Resume game");
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Debug.Log("Restart game");
+    }
+}
